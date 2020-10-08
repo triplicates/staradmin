dda04c20dacfafb79acf95560aa44b5a3ade8386
triplicates <triplicatesouls@gmail.com>
2020-10-08 21:58:04 +0400

    [update] Some updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom"
-import {BrowserRouter as Router, Switch, Route} from "react-router"
 import "./components/Index/remove.css"
 import Home from "./components/Home/Home.jsx"
+
+
 
 ReactDOM.render(<Home />,document.querySelector('#root'))
